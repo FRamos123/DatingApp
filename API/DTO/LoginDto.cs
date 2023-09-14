8c8b508b5f78eaa1f8c62334bf0af96cb4c12155
Framos123 <alframoscastr@gmail.com>
2023-09-14 17:18:47 +0200

    Added token authentication and extensions

---
+++ API/DTO/LoginDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.DTO
+{
+    public class LoginDto
+    {
+   public string Username { get; set; }
+
+   public string Password { get; set; }
+    }
+}
