70994674c8fae7fcaa8e0b1f011d2593ed6961bf
Framos123 <alframoscastr@gmail.com>
2023-09-20 14:48:04 +0200

    Error handling components and interceptor

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -9,10 +9,11 @@ namespace API.DTO
     public class RegisterDto
     {
         [Required]
-        public string Username {get;set;}
+        public string Username { get; set; }
 
-        
+
         [Required]
-              public string Password {get;set;}
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; }
     }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -31,10 +31,7 @@ export class NavComponent implements OnInit {
         this.router.navigateByUrl('/members');
 
       },
-      error: error => {
-        console.log(error);
-        this.toastr.error(error.error);
-      }
+     
 
     });
   }
